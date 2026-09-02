64e32052b05963af35b957a73bb7f4ee11b876a9
MD AHAD ALI <96808092+Iam-abdulahad@users.noreply.github.com>
2026-09-02 09:35:14 +0600

    started learning next js

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import AcmeLogo from "@/app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">Tailwind is working!</h1>
+    </main>
+  );
+}
